1ddbb51c1b6f63ab39f388a6a4bb484b8b51658a
Руслан Хасанов <ruslanhq@MacBook-Pro.local>
2021-04-19 18:21:50 +0600

    added model groups

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from authentication.models import User
+from authentication.models import User, Groups
 
 admin.site.register(User)
-
+admin.site.register(Groups)
--- authentication/models.py
+++ authentication/models.py
@@ -32,6 +32,15 @@ class UserManager(BaseUserManager):
         return user
 
 
+class Groups(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    params = models.JSONField(default=dict)
+
+    def __str__(self):
+        return self.title
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     email = models.EmailField(db_index=True, unique=True)
@@ -39,6 +48,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    groups = models.ManyToManyField(Groups, related_name='users')
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
