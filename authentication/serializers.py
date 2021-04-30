16dacd668083a5e8c1621068c234c727c95ef8cf
ruslanhq <h.ruslan97@gmail.com>
2021-04-30 07:14:37 +0000

    Ticket #33 implement jwt authorization

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from rest_framework import status
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from authentication.fields import RPCEndpointField
 from authentication.models import User, Groups
@@ -15,15 +16,40 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserGroupsSerializer(serializers.ModelSerializer):
-    groups = serializers.SlugRelatedField(many=True, slug_field='title',
-                                          queryset=Groups.objects.all())
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.filter(is_active=True)
+    )
+    groups = serializers.SlugRelatedField(
+        many=True, slug_field='title',
+        queryset=Groups.objects.filter(is_active=True)
+    )
 
     class Meta:
         model = User
-        fields = ('groups',)
+        fields = ('user_id', 'groups',)
 
 
 class UserResponseSerializer(serializers.Serializer):
     user = UserSerializer(read_only=True, source='*')
     endpoint = RPCEndpointField(read_only=True, source='*')
     http_status = serializers.IntegerField(default=status.HTTP_200_OK)
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password',)
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        return token
