59c08909819dddf222298ac67d8fe14041676618
ruslanhq <h.ruslan97@gmail.com>
2021-05-19 06:40:01 +0000

    Ticket-#37 add change password view

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from authentication.views import UserViewSet, CustomTokenObtainPairView
+from authentication.views import (
+    UserViewSet, CustomTokenObtainPairView, ChangePasswordView
+)
 
 app_name = 'authentication'
 
@@ -12,6 +14,7 @@ router.register('user', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('change_password/', ChangePasswordView.as_view()),
     path('token/', CustomTokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh'),
